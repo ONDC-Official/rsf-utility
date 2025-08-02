297cf31ce4c3df9e3d2a460fc2e42f25c437dec0
extedcoud <extedcoud@gmail.com>
2025-08-03 02:06:26 +0530

    swagger-ui

--- src/docs/open-apiSpec.ts
+++ src/docs/open-apiSpec.ts
@@ -5,35 +5,15 @@ import {
 } from "@asteasolutions/zod-to-openapi";
 
 import { z } from "zod";
-import { GetSettlementsQuerySchema } from "../types/settle-params";
+import {
+	GetSettlementsQuerySchema,
+	PrepareSettlementsBody,
+} from "../types/settle-params";
 import { UserSchema } from "../schema/models/user-schema";
+import { SettleSchema } from "../schema/models/settle-schema";
+import { objectIdSchema } from "../types/user-id-type";
 // Create a registry to collect all our definitions
 const registry = new OpenAPIRegistry();
-
-registry.registerPath({
-	method: "get",
-	path: "/ui/settle/{userId}",
-	summary: "Retrieve a list of settlements",
-	request: {
-		query: GetSettlementsQuerySchema,
-		params: z.object({
-			userId: z.string().openapi({ description: "The ID of the user config" }),
-		}),
-	},
-	responses: {
-		200: {
-			description: "A list of settlements.",
-			content: {
-				"application/json": {
-					// Define a schema for the response body
-					schema: z.object({
-						message: z.string().openapi({ example: "Success!" }),
-					}),
-				},
-			},
-		},
-	},
-});
 
 registry.registerPath({
 	method: "get",
@@ -54,6 +34,112 @@ registry.registerPath({
 	},
 });
 
+registry.registerPath({
+	method: "post",
+	path: "/ui/user/",
+	summary: "Create a new user config",
+	request: {
+		body: {
+			content: {
+				"application/json": {
+					schema: UserSchema,
+				},
+			},
+		},
+	},
+	responses: {
+		201: {
+			description: "User config created successfully.",
+			content: {
+				"application/json": {
+					schema: UserSchema.extend({
+						_id: z.string().openapi({ description: "MongoDB Object ID" }),
+					}),
+				},
+			},
+		},
+	},
+});
+
+registry.registerPath({
+	method: "patch",
+	path: "/ui/user/{userId}",
+	summary: "Update a user config",
+	request: {
+		params: z.object({
+			userId: objectIdSchema,
+		}),
+		body: {
+			content: {
+				"application/json": {
+					schema: UserSchema.partial(),
+				},
+			},
+		},
+	},
+	responses: {
+		200: {
+			description: "User config updated successfully.",
+			content: {
+				"application/json": {
+					schema: UserSchema,
+				},
+			},
+		},
+	},
+});
+
+registry.registerPath({
+	method: "get",
+	path: "/ui/settle/{userId}",
+	summary: "Retrieve a list of settlements for a user",
+	request: {
+		query: GetSettlementsQuerySchema,
+		params: z.object({
+			userId: objectIdSchema,
+		}),
+	},
+	responses: {
+		200: {
+			description: "A list of settlements.",
+			content: {
+				"application/json": {
+					// Define a schema for the response body
+					schema: SettleSchema.array(),
+				},
+			},
+		},
+	},
+});
+
+registry.registerPath({
+	method: "post",
+	path: "/ui/settle/{userId}/prepare",
+	summary: "Prepare a settlements for a user for given order ids",
+	request: {
+		params: z.object({
+			userId: objectIdSchema,
+		}),
+		body: {
+			content: {
+				"application/json": {
+					schema: PrepareSettlementsBody,
+				},
+			},
+		},
+	},
+	responses: {
+		200: {
+			description: "Settlements prepared successfully.",
+			content: {
+				"application/json": {
+					schema: SettleSchema.array(),
+				},
+			},
+		},
+	},
+});
+
 // Generate the OpenAPI document from the registry
 const generator = new OpenApiGeneratorV31(registry.definitions);
 
@@ -61,8 +147,8 @@ export const openApiDocument = generator.generateDocument({
 	openapi: "3.1.0",
 	info: {
 		version: "1.0.0",
-		title: "My Settlement API",
-		description: "API for managing settlements",
+		title: "ondc-rsf-utility API",
+		description: "backend APIs for ondc-rsf-utility",
 	},
 	servers: [{ url: "/" }],
 });
--- src/types/user-id-type.ts
+++ src/types/user-id-type.ts
@@ -3,7 +3,11 @@ import { z } from "zod";
 export const objectIdSchema = z
 	.string()
 	.length(24)
-	.regex(/^[0-9a-fA-F]{24}$/, "Invalid user ID format");
+	.regex(/^[0-9a-fA-F]{24}$/, "Invalid user ID format")
+	.openapi({
+		description: "Unique identifier for the user config",
+		example: "60c72b2f9b1e8b001c2f8b1e",
+	});
 
 export type UserIdType = z.infer<typeof objectIdSchema>;
 
