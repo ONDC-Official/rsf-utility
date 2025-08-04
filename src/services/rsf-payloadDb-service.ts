16bf57b1057b5a165c5661547af21929a16b57a7
extedcouD <extedcoud@gmail.com>
2025-08-05 00:42:51 +0530

    changes

--- src/services/rsf-payloadDb-service.ts
+++ src/services/rsf-payloadDb-service.ts
@@ -3,14 +3,25 @@ import {
 	GetRsfPayloadsParamsType,
 	RsfPayloadStructure,
 } from "../types/rsf-payloads-params";
+import logger from "../utils/logger";
 
+const rsfPayloadLogger = logger.child("rsf-payload-db-service");
 export class RsfPayloadDbService {
 	constructor(private rsfPayloadRepository: RsfPayloadRepository) {}
 	getRsfPayloads = async (params: GetRsfPayloadsParamsType) => {
-		return this.rsfPayloadRepository.getRsfPayloads(params);
+		try {
+			return this.rsfPayloadRepository.getRsfPayloads(params);
+		} catch (error) {
+			rsfPayloadLogger.error("Error fetching Rsf Payloads", { params }, error);
+		}
 	};
 
 	saveRsfPayload = async (rsfPayload: RsfPayloadStructure) => {
-		return this.rsfPayloadRepository.createRsfPayload(rsfPayload);
+		try {
+			const item = await this.rsfPayloadRepository.createRsfPayload(rsfPayload);
+			return item;
+		} catch (error) {
+			rsfPayloadLogger.error("Error saving Rsf Payload", {}, error);
+		}
 	};
 }
