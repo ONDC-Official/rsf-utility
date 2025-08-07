632abc7db5e14637209c52edbf19eafd4930f3a1
sandeepshahi <s.sandeepshahi@gmail.com>
2025-08-07 18:22:54 +0530

    fix: update api-docs for auth handle

--- src/controller/auth-controller.ts
+++ src/controller/auth-controller.ts
@@ -7,11 +7,11 @@ import { sendError, sendSuccess } from "../utils/resUtils";
 
 const authController = {
 	signToken: async (req: Request, res: Response) => {
-		const { client_id, expiresIn } = req.body; //client id from request body
+		const { client_id, expires } = req.body; //client id from request body
 		const { CLIENT_ID } = config; //Saved CLIENT_ID
 		const authService = new AuthService(CLIENT_ID);
 
-		const token = authService.issueToken(client_id, expiresIn);
+		const token = authService.issueToken(client_id, expires);
 
 		if (!token) {
 			return sendError(res, "INVALID_CLIENT_ID", "Invalid client_id");
