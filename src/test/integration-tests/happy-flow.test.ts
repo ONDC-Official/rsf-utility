6a31823e981a90ea57cf3cfb76edb040ecc1d6ee
extedcoud <extedcoud@gmail.com>
2025-08-08 23:33:29 +0530

    changse

--- src/test/integration-tests/happy-flow.test.ts
+++ src/test/integration-tests/happy-flow.test.ts
@@ -1,7 +1,39 @@
-/*
-    1. Create a new user
-    2. create a order 
-    3. prepare a settlement
-    4. generate & trigger a settlement
-    5. get on_settle with "SETTLED"
-*/
+// /*
+//     1. Create a new user
+//     2. create a order
+//     3. prepare a settlement
+//     4. generate & trigger a settlement
+//     5. get on_settle with "NOT_SETTLED"
+//     6. generate & trigger recon
+//     7. get on_recon
+//     8. move to ready
+// */
+// import request from "supertest";
+// import { createServer } from "http";
+// import { UserType } from "../../schema/models/user-schema";
+
+// describe("Happy Flow Integration Tests", () => {
+// 	const app = createServer();
+// 	const userData: UserType = {
+// 		title: "TEST_DOMAIN",
+// 		role: "BAP",
+// 		domain: "ONDC:RET10",
+// 		subscriber_url: "http://test-subscriber.com",
+// 		np_tcs: 123,
+// 		np_tds: 456,
+// 		pr_tcs: 789,
+// 		pr_tds: 23,
+// 		msn: false,
+// 		provider_details: [
+// 			{
+// 				provider_id: "provider123",
+// 				account_number: "1234567890",
+// 				ifsc_code: "IFSC1234",
+// 				bank_name: "Bank ABC",
+// 			},
+// 		],
+// 		counterparty_ids: [],
+// 	};
+
+// 	const userResponse = await request(app).
+// });
